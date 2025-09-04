a269d867db3bd111e1e8cf75b6f8452e357d6205
baileytorch <baileytorch@hotmail.com>
2025-09-04 16:24:18 -0400

    Crear serializers

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Nacionalidad, Autor, Comuna, Direccion
+from .models import Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Lector, TipoCategoria, Categoria, Libro, Prestamo
 
 # Register your models here.
 admin.site.register(Nacionalidad)
 admin.site.register(Autor)
 admin.site.register(Comuna)
 admin.site.register(Direccion)
+admin.site.register(Biblioteca)
+admin.site.register(Lector)
+admin.site.register(TipoCategoria)
+admin.site.register(Categoria)
+admin.site.register(Libro)
+admin.site.register(Prestamo)
